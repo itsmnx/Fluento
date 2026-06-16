eee681c36dddd6c7d2bda16f78cb13559aa97002
MANAS JOSHI <158481955+itsmnx@users.noreply.github.com>
2026-06-16 19:16:13 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = 
   import.meta.env.VITE_API_URL || 
-  "https://fluentoapp.onrender.com/api";
+  "https://fluentoapp.onrender.com/api/auth"; 
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
